f0ff7122691b09314a3b119282ea208e6629039e
Paul <hhuang8311tw@gmail.com>
2020-07-31 13:17:57 +0800

    correcting mistakingly put answer on server

--- Validate_Subsequence.p2/validate_subsequence.py
+++ Validate_Subsequence.p2/validate_subsequence.py
@@ -3,13 +3,6 @@
 def isValidSubsequence(array, sequence):
     # Write your code here.
 
-    arrIdx = 0
-    seqIdx = 0
-    while arrIdx < len(array) and seqIdx < len(sequence):
-        if array[arrIdx] == sequence[seqIdx]:
-            seqIdx += 1
-        arrIdx += 1
-    return seqIdx == len(sequence)
 
 
 
